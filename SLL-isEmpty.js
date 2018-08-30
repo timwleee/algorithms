b1e6be9ecb592df030b366c5c40344acd37099ce
Tim Lee <leewtim@gmail.com>
2018-08-29 18:04:18 -0700

    Added SLL-isEmpty

---
+++ SLL-isEmpty.js
@@ -0,0 +1,28 @@
+function LinkedList () {
+    this.head = null;
+}
+
+function Node(value, next, prev) {
+    this.value = value;
+    this.next = next;
+    this.prev = prev;
+}
+
+LinkedList.prototype.addNode = function(value) {
+    const newNode = new Node(value, this.head, null);
+    if (this.head) this.head.prev = newNode;
+    else this.tail = newNode; 
+    this.head = newNode;
+};
+
+const list = new LinkedList();
+list.addNode(5);
+list.addNode(64);
+list.addNode(15);
+
+LinkedList.prototype.isEmpty = function(){ 
+    if (this.head === null) console.log ('Empty');
+    else console.log ('Not empty');
+};
+
+list.isEmpty();
