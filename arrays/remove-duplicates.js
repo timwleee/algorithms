2ad5b58c44fb03554984b63936f27793ac3123c1
Tim Lee <leewtim@gmail.com>
2018-09-08 22:44:16 -0700

    Edited arrays/remove-duplicates

--- arrays/remove-duplicates.js
+++ arrays/remove-duplicates.js
@@ -1,1 +1,18 @@
 // Given a sorted array, remove duplicate values. Do this without using any built-in array methods.
+
+function removeDuplicates() {
+    var arr = [8,46,5,64,9,5,27];
+    var temp;
+    for (var i = 0; i < arr.length; i++) {
+        temp === arr[i];
+    }
+        for (var j = 1; j < arr.length; j++) {
+            if (temp === j) {
+                arr.splice(i,1);
+                arr.splice(j,1);
+            }
+        }
+    console.log(arr);
+};
+
+removeDuplicates();
