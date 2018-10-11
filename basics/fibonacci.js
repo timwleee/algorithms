5c46715532ed3bde71dc5e591e3a4e4daf7574dc
Tim Lee <leewtim@gmail.com>
2018-10-10 23:48:10 -0700

    basics/fibonacci

---
+++ basics/fibonacci.js
@@ -0,0 +1,15 @@
+// Write a function that returns the nth fibonnaci number
+
+function fibonacci(n){
+    var fibo = [0, 1];
+    
+    if (n <= 2) return 1;
+  
+    for (var i = 2; i <=n; i++ ){
+        fibo[i] = fibo[i-1]+fibo[i-2];
+    }
+  
+   return fibo[n];
+  } 
+  
+console.log(fibonacci(12));
