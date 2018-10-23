efe0b1aeb7f5e5df8bb44f0ec0fcc19574b59a32
Tim Lee <leewtim@gmail.com>
2018-10-22 23:46:04 -0700

    basics/check if palindrome

---
+++ basics/checkPalindrome.js
@@ -0,0 +1,13 @@
+// Write a function that accepts a string and returns a boolean to check if it is a palindrome
+
+function palindrome(str) {
+    for (let i = 0; i < str.length/2; i++) {
+        if (str[i] !== str[str.length-1-i]) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+
+console.log(palindrome('racecar'));
