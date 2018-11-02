5d63ef52ae24c598a292428d3c170c7653cac7bb
Tim Lee <leewtim@gmail.com>
2018-11-01 23:48:48 -0700

    Find pair of numbers that equals the given sum

---
+++ basics/findSumOfPair.js
@@ -0,0 +1,13 @@
+// Given a number and an array, find a pair of numbers in an unsorted array that equals the given sum
+
+function findSum(arr, sum) {
+    for(i = 0; i < arr.length-1; i++){  
+        for(j = i + 1; j < arr.length; j++){
+           if (arr[i] + arr[j] == sum)
+               console.log(arr[i], arr[j]);
+        }
+     }
+     return false;
+}
+
+findSum([6,4,3,2,1,7], 9)
